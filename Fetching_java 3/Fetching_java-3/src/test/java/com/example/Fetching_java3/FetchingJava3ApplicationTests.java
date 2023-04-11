a84ac615f719bf57809224042dc95eb4ea6b1f31
Jay01789 <102054579+Jay01789@users.noreply.github.com>
2023-04-11 18:53:46 +0530

    Add files via upload

---
+++ Fetching_java 3/Fetching_java-3/src/test/java/com/example/Fetching_java3/FetchingJava3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fetching_java3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchingJava3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
