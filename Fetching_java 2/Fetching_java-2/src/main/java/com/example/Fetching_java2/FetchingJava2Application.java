6020d10984a53f20e687a6a4f8da9ad8dc0165dc
Jay01789 <102054579+Jay01789@users.noreply.github.com>
2023-04-11 18:32:15 +0530

    Add files via upload

---
+++ Fetching_java 2/Fetching_java-2/src/main/java/com/example/Fetching_java2/FetchingJava2Application.java
@@ -0,0 +1,41 @@
+package com.example.Fetching_java2;
+
+import org.json.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FetchingJava2Application {
+
+	public static void main(String[] args) throws IOException {
+
+		URL getUrl = new URL("https://api.zippopotam.us/us/33162	");
+		HttpURLConnection connection = (HttpURLConnection) getUrl.openConnection();
+		connection.setRequestMethod("GET");
+		int responseCode = connection.getResponseCode();
+
+		if(responseCode == 200){
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			StringBuilder jsonResponseData = new StringBuilder();
+			String readLine = null ;
+
+			while((readLine = in.readLine()) != null){
+				jsonResponseData.append(readLine);
+			}
+			in.close();
+			JSONObject masterData = new JSONObject(jsonResponseData.toString());
+		System.out.print(masterData);
+
+
+		}else{
+			System.out.println("Error , Check Connection");
+		}
+
+	}
+
+}
---
+++ Fetching_java 2/Fetching_java-2/src/test/java/com/example/Fetching_java2/FetchingJava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fetching_java2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchingJava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
